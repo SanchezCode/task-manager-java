4d9a52f29cf0791167ed70cac80d5a76b253cf4f
Santiago :v <santiagosancheznieva@gmail.com>
2025-03-03 21:11:42 -0300

    Rename TaskManager to Main

--- src/com/taskmanager/Main.java
+++ src/com/taskmanager/Main.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 import com.taskmanager.models.Task;
 
-public class TaskManager {
+public class Main {
 
 	public static void main(String[] args) {
 		Task task1 = new Task("Hacer ejercicio", "Tengo que ir al gym si quiero estar en forma", new Date(), 1);
