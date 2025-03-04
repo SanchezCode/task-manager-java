11514de7d464e65e58aa9e2cd18b1d98120824a3
Santiago :v <santiagosancheznieva@gmail.com>
2025-03-03 22:12:03 -0300

    Add basic menu to manage tasks
    Will be improved in the future

--- src/com/taskmanager/Main.java
+++ src/com/taskmanager/Main.java
@@ -1,17 +1,62 @@
 package com.taskmanager;
 
 import java.util.Date;
+import java.util.Scanner;
 
 import com.taskmanager.models.Task;
+import com.taskmanager.services.TaskManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Task task1 = new Task("Hacer ejercicio", "Tengo que ir al gym si quiero estar en forma", new Date(), 1);
-		System.out.print(task1.toString());	
-		task1.completeTask();
-		System.out.println("Tarea completada!");
-		System.out.print(task1.toString());	
+		TaskManager taskManager = new TaskManager();
+		Scanner input = new Scanner(System.in);
+		while(true) {
+            System.out.println("\n--- Menú ---");
+            System.out.println("1. Listar tareas");
+            System.out.println("2. Crear nueva tarea");
+            System.out.println("3. Eliminar tarea");
+            System.out.println("4. Completar tarea");
+            System.out.println("5. Salir");
+            System.out.print("Selecciona una opción: ");
+            int option = input.nextInt();
+            input.nextLine();  // Limpiar el buffer
+            switch(option) {
+            case 1: 
+            	taskManager.listTasks();
+            	break;
+            case 2:
+            	System.out.println("Titulo de la tarea: ");
+            	String title = input.nextLine();
+            	System.out.println("Descripcion: ");
+            	String description = input.nextLine();
+            	System.out.println("Fecha de vencimiento (formato yyyy-mm-dd): ");
+            	String dueDateString = input.nextLine();
+            	Date dueDate = new Date();
+            	System.out.println("Prioridad (1 - Alta, 2 - Media, 3 - Baja): ");
+            	int priority = input.nextInt();
+            
+            	taskManager.addTask(title, description, dueDate, priority);
+            	System.out.println("Tarea creada.");
+            	break;
+            case 3:
+            	System.out.println("Ingresar nombre de la tarea: ");
+            	String removeTaskTitle = input.nextLine();
+            
+            	taskManager.deleteTask(removeTaskTitle);
+            	break;
+            case 4:
+            	System.out.println("Ingresar nombre de la tarea completada: ");
+            	String completedTaskTitle = input.nextLine();
+            	taskManager.completeTask(completedTaskTitle);
+            	break;
+            case 5:
+            	System.out.println("Saliendo...");
+            	return;
+            default:
+            	System.out.println("Opcion invalida.");
+            }
+		}
 	}
 
 }
